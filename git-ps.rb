6abb01713f7099024870b39480e6e56883e324dc
Jan Aerts <jan.aerts@gmail.com>
2010-04-19 20:01:39 +0100

    Using yellow if changes added but not committed

--- git-ps.rb
+++ git-ps.rb
@@ -1,15 +1,22 @@
-green = "\033[0;32m"
-reset = "\033[0m"
-red   = "\033[0;31m"
+green  = "\033[1;32m"
+yellow = "\033[1;33m"
+red    = "\033[1;31m"
+blue   = "\033[1;34m"
+reset  = "\033[0m"
+
 current_branch = `git branch 2>/dev/null`.grep(/^\*/).first
 if current_branch
   branch_name = current_branch.gsub(/^\*\s*/,'').strip
-
+STDERR.puts "DEBUG: " + branch_name
   status_lines = `git status`.split(/\n/)
-  status = ( status_lines.length == 2 ) ? 'clean' : 'dirty'
-  color = ( status == 'clean' ) ? green : red
+  colour = green
+  if status_lines.grep(/^# Changed but not updated/).length > 0
+    colour = red
+  elsif status_lines.grep(/^# Changes to be committed/).length > 0
+    colour = yellow
+  end
   unless current_branch.empty?
-    print "#{color}(#{branch_name})#{reset}"
+    print "#{colour}(#{branch_name})#{reset}"
   end
 end
 
